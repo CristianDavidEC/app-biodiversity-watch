8481791507ab181761fbaf267a6b132f72e34ebb
CristianDavidEC <you@example.com>
2025-04-01 15:39:26 -0500

    run project base, and add icons to tabBar

--- biodiversity-watch/app/(tabs)/_layout.tsx
+++ biodiversity-watch/app/(tabs)/_layout.tsx
@@ -1,12 +1,10 @@
 import { Tabs } from 'expo-router';
 import React from 'react';
 import { Platform } from 'react-native';
-
-import { HapticTab } from '@/components/HapticTab';
-import { IconSymbol } from '@/components/ui/IconSymbol';
 import TabBarBackground from '@/components/ui/TabBarBackground';
 import { Colors } from '@/constants/Colors';
 import { useColorScheme } from '@/hooks/useColorScheme';
+import { HomeIcon, CameraIcon, ProfileIcon } from '@/components/common/icons';
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
@@ -15,12 +13,10 @@ export default function TabLayout() {
     <Tabs
       screenOptions={{
         tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
         tabBarBackground: TabBarBackground,
+        headerShown: false,
         tabBarStyle: Platform.select({
           ios: {
-            // Use a transparent background on iOS to show the blur effect
             position: 'absolute',
           },
           default: {},
@@ -30,14 +26,21 @@ export default function TabLayout() {
         name="index"
         options={{
           title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          tabBarIcon: ({ color }) => <HomeIcon color={color} />,
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="camera"
         options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          title: 'Camera',
+          tabBarIcon: ({ color }) => <CameraIcon color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: 'Profile',
+          tabBarIcon: ({ color }) => <ProfileIcon color={color} />,
         }}
       />
     </Tabs>
---
+++ biodiversity-watch/app/(tabs)/profile.tsx
@@ -0,0 +1,7 @@
+import { Text } from 'react-native';
+
+export default function Profile() {
+  return (
+    <Text className='text-2xl text-red-500'>Page del perfil</Text>
+  );
+}
